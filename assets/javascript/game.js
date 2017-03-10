d7d415751001bccd04bbf7d735fe92e87698a561
canlas1 <dc@uscr.com>
2017-03-10 00:55:06 -0500

    push5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,121 +1,109 @@
 // A $( document ).ready() block.
 $(document).ready(function() {
-    console.log( "ready!" );
+    console.log("ready!");
 
-// Global Variables refer to in any function
+    // Global Variables refer to in any function
 
-//start at 0 for the purpose of the function
-//var ranNumGreen = 0;
-//console.log(ranNumGreen)
-//var ranNumBlue = 0;
-//var ranNumYellow = 0;
-//var ranNumRed = 0;
-//var ranNumGen = 0; //look to demo started with example 39
-var wins = 0;
-var losses = 0;
-var ranTotNum = 0;
-//var totNum = 0;
- //this is what needs to match the ranNumGen
-// Min and Max rules total and click per button //random button number b/w 1 and 12
-var minRanNumBtn = 0;
-var maxRanNumBtn = 12;
-var minRanTotNum = 19;
-var maxRanTotNum = 120;
-
-var startGame = function(){ 		//local variables inside startGame Function to set random number 0 to 12 per button 
-	ranNumBlue = Math.floor((Math.random() * 12) + 1);//1-12 per click on button
-	ranNumGreen = Math.floor((Math.random() * 12) + 1);
-	ranNumRed = Math.floor((Math.random() * 12) + 1);
-	ranNumYellow = Math.floor((Math.random() * 12) + 1);
-	ranNumGen = Math.floor((Math.random() * 101) + 19);//start 19-120 random
-	$("#ranNumGen").html(ranNumGen);
-	userTotalScore = 0
-	$("#userTotalScore").html(" " + userTotalScore);
-			
-
-			console.log(ranNumBlue);
-			console.log(ranNumGreen);
-			console.log(ranNumYellow);
-			console.log(ranNumGen);
-			};
-			startGame();
+    //start at 0 for the purpose of the function
+    //var ranNumGreen = 0;
+    //console.log(ranNumGreen)
+    //var ranNumBlue = 0;
+    //var ranNumYellow = 0;
+    //var ranNumRed = 0;
+    //var ranNumGen = 0; //look to demo started with example 39
 
 
-/// DIVS that i need to grab 
-//<div id="totalScoreIs">
-//<div id="totalScoreNum">
-//<div class="col-md-8" id="ranNumGen">
-//<img src="assets/images/redCrystal.png" alt="..." id="redCrystal">
-//<img src="assets/images/blueCrystal.png" alt="..." id="blueCrystal">
-//<img src="assets/images/yellowCrystal.png" alt="..." id="yellowCrystal">
-//<img src="assets/images/greenCrystal.png" alt="..." id="greenCrystal">
-//<li id="wins">
-//<li id="loses">
+    /// DIVS that i need to grab 
+    //<div id="totalScoreIs">
+    //<div id="totalScoreNum">
+    //<div class="col-md-8" id="ranNumGen">
+    //<img src="assets/images/redCrystal.png" alt="..." id="redCrystal">
+    //<img src="assets/images/blueCrystal.png" alt="..." id="blueCrystal">
+    //<img src="assets/images/yellowCrystal.png" alt="..." id="yellowCrystal">
+    //<img src="assets/images/greenCrystal.png" alt="..." id="greenCrystal">
+    //<li id="wins">
+    //<li id="loses">
+
+    var wins = 0;
+    var losses = 0;
+    var ranTotNum = 0;
+    //var totNum = 0;
+    //this is what needs to match the ranNumGen
+    // Min and Max rules total and click per button //random button number b/w 1 and 12
+    // var minRanNumBtn = 0;
+    // var maxRanNumBtn = 12;
+    // var minRanTotNum = 19;
+    // var maxRanTotNum = 120;
+
+    var startGame = function() { //local variables inside startGame Function to set random number 0 to 12 per button 
+        ranNumBlue = Math.floor((Math.random() * 12) + 1); //1-12 per click on button
+        ranNumGreen = Math.floor((Math.random() * 12) + 1);
+        ranNumRed = Math.floor((Math.random() * 12) + 1);
+        ranNumYellow = Math.floor((Math.random() * 12) + 1);
+        ranNumGen = Math.floor((Math.random() * 101) + 19); //start 19-120 random
+        $("#ranNumGen").html(ranNumGen);
+        $("#wins").html(wins);
+        $("#losses").html(losses);
+        userTotalScore = 0;
+        $("#userTotalScore").html(" " + userTotalScore);
 
 
- //==========================================================================================
-// //start a JQuery function when the player clicks a crystal, generate a random number based on that click and add it to the players totNum
+        console.log(ranNumBlue);
+        console.log(ranNumGreen);
+        console.log(ranNumRed);
+        console.log(ranNumYellow);
+        console.log(ranNumGen);
+        console.log(userTotalScore);
+    };
+    startGame(); // need explination here???
 
-// $("#blueCrystal").on("click", function(){
-// 	if (ranNumBlue){
+    // function startGame(){ // dont know why i need to end this function here?????
 
-//  		totNum = totNum + ranNumBlue;
-//  		$("#ranNumGen").html(totNum);}
+    console.log(startGame);
+    console.log("is this working!!!!!!")
 
+    var logic = function() {
+        if (userTotalScore === ranNumGen) {
+            wins = wins + 1;
+            startGame();
+        } else if (userTotalScore > ranNumGen) {
+            losses = losses + 1;
+            startGame();
+        } else {
+            userTotalScore;
+            console.log(userTotalScore);
 
-//  		console.log("is this happening");
-//  	else{
-//  		ranNumBlue = Math.floor((Math.random() * 12) + 1);
-//  ;}
+        }
+    };
+    logic();
 
-//  console.log(ranNumBlue);
+    $("#blueCrystal").on("click", function() {
+        console.log($(this).html());
+        userTotalScore = userTotalScore + ranNumBlue;
+        console.log(userTotalScore);
+        logic();
 
-// });
- });
+    })
 
+    $("#yellowCrystal").on("click", function() {
+        console.log($(this).html());
+        userTotalScore = userTotalScore + ranNumYellow;
+        console.log(userTotalScore);
+        logic();
+    })
 
+    $("#greenCrystal").on("click", function() {
+        console.log($(this).html());
+        userTotalScore = userTotalScore + ranNumGreen;
+        console.log(userTotalScore);
+        logic();
+    })
 
+    $("#redCrystal").on("click", function() {
+        console.log($(this).html());
+        userTotalScore = userTotalScore + ranNumRed;
+        console.log(userTotalScore);
+        logic();
+    })
 
-
-
-
-
-
-
-
-// // Inside, we latch onto the "emptyDiv" using the JavaScript ".getElementById" selector.
-//       var newRanNumGen = document.getElementById("#ranNumGen");
-//       // We then use the JavaScript method ".innerHTML" to change the content to "Hello friends!"
-//       newRanNumGen.innerHTML = "Hello friends!";
-
-//       var newWins = document.getElementById("#wins");
-//         wins.innerHTML = "Hello friends! WINS";
-    
-//     var newLosses = document.getElementById("#losses");
-//         losses.innerHTML = "Hello friends! LOSSES";
-
-// var ranNumGen
-
-// var wins = 
-// var losses
-// var totalScore
-// var totalScoreBox
-
-
-
-// blueCrystal
-// greenCrystal
-// redCrystal
-// yellowCrystal
-
-
-
-// //Functions
-
-
-
-
-// // Main Process
-// //==============================================================================================
-
-
+});
