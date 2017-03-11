bfea473cfe2912961de4383f4e6e8b51d0165bbf
canlas1 <dc@uscr.com>
2017-03-10 19:06:36 -0500

    finalcommit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,11 @@
 $(document).ready(function() {
     console.log("ready!");
 
-    // Global Variables & Psuedo refer to in any function
-
-    //start at 0 for the purpose of the function
-    //var ranNumGreen = 0;
-    //console.log(ranNumGreen)
-    //var ranNumBlue = 0;
-    //var ranNumYellow = 0;
-    //var ranNumRed = 0;
-    //var ranNumGen = 0; //look to demo started with example 39
-    /// DIVS that i need to grab 
-    //<div id="userTotalScore">
-    //<div class="col-md-8" id="ranNumGen">
-    //<img src="assets/images/redCrystal.png" alt="..." id="redCrystal">
-    //<img src="assets/images/blueCrystal.png" alt="..." id="blueCrystal">
-    //<img src="assets/images/yellowCrystal.png" alt="..." id="yellowCrystal">
-    //<img src="assets/images/greenCrystal.png" alt="..." id="greenCrystal">
-    //<h4 id="wins">
-    //<h4 id="loses">
-
     var wins = 0;
     var losses = 0;
     var ranTotNum = 0;
 
-    var startGame = function() { //local variables inside startGame Function to set random number 0 to 12 per button 
+    function startGame() { 
         ranNumBlue = Math.floor((Math.random() * 12) + 1); //1-12 per click on button
         ranNumGreen = Math.floor((Math.random() * 12) + 1);
         ranNumRed = Math.floor((Math.random() * 12) + 1);
@@ -39,11 +20,12 @@ $(document).ready(function() {
             console.log(ranNumGen);
             console.log(userTotalScore);
     };
-    startGame(); // this worked better for me on the global scope.  I read it is better practice for to pass the varibles through the function and make them local but i found more logical to define as global variables.  So i can restart loops  
+
+    startGame(); // ?? i am trying to call this outside the main function document.ready to make it easier to read the code but i was getting a NaN and ObjectHTML on my targets
         console.log(startGame);
         console.log("is this working!!!!!!")
 
-    var logic = function() {
+    function logic() {
         if (userTotalScore === ranNumGen) {
             wins++;
             $("#wins").html("Wins: " + wins);
@@ -64,7 +46,8 @@ $(document).ready(function() {
             console.log(userTotalScore);
         }
     };
-    logic(); // this worked better for me on the global scope.  I read it is better practice for to pass the varibles through the function and make them local but i found more logical to define as global variables.  So i can restart loops  
+    
+    logic(); //same thing here 
 
     $("#blueCrystal").on("click", function() {
         console.log($(this).html());
